807d54fde3d55a39ad70ddca0e1d4dab9e085917
Mauricio Hippert Teixeira <mauriciohippertt@gmail.com>
2022-09-08 17:51:11 -0500

    Initial commit

---
+++ exec/build_isobars.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 31 16:33:43 2022
+
+@author: mauricio
+
+Usage: ./make_isobar_seeds.py conf.yaml
+
+Creates hdf5 file with shape (number_configs, number_nucleons, 2) 
+with random numbers to generate isobar configurations
+"""
+
+import numpy as np
+import h5py
+import sys
+import os
+import yaml
+# import numpy.random as random
+from yaml.loader import SafeLoader
+
+
+N_SEEDS = 3 + 3 # 3 for r, theta, phi + 3 for delta_{x,y,z} Gaussian
+POS_SEEDS = {'radius':0,'costheta':1,'phi':2,'gauss_x':3,'gauss_y':4,'gauss_z':5 }
+GAUSS_SEEDS = ['gauss_x','gauss_y','gauss_z'] # Gaussian seeds
+UNIFORM_SEEDS = ['radius','costheta','phi']
+#%%
+
+def sph_harmonic_2(costheta,phi):
+    return np.sqrt(5./(4.*np.pi))*(3.*costheta**2 -1)/2.
+
+def int_sph_harmonic_2(costheta,phi):
+    return (np.sqrt(5./np.pi)/4.)*(costheta**3 -costheta)
+
+def sph_harmonic_3(costheta,phi):
+    return np.sqrt(7./(4.*np.pi))*(5.*costheta**3 -3.*costheta)/2.
+    
+def int_sph_harmonic_3(costheta,phi):
+    return (np.sqrt(7./np.pi)/4.)*(5.*costheta**4/4. - 3.*costheta**2/2.)
+
+def deform(r,costheta,phi,beta2,beta3):
+    r = r*(1. + beta2*sph_harmonic_2(costheta, phi) + beta3*sph_harmonic_3(costheta, phi))
+    costheta = costheta - 3.*(beta2*int_sph_harmonic_2(costheta, phi) + beta3*int_sph_harmonic_3(costheta, phi))
+    phi = phi
+    
+    return r, costheta, phi
+
+def cartesian(r,costheta,phi):
+    sintheta = np.sqrt(1.-costheta**2)
+    z = r*costheta
+    x = r*sintheta*np.cos(phi)
+    y = r*sintheta*np.sin(phi)
+    
+    return x, y, z
+#%% 
+def place_nucleon(R_step, w_gauss, beta2, beta3, seed):
+    # radial coordinate
+    radius_seed = seed[POS_SEEDS['radius']]
+    # p(r) ~ r²dr, cdf(r) ~ r³, r ~ seed_r^(1/3), cdf sampled uniform [0,1]
+    # plus max radius is R_step
+    r = radius_seed**(1./3.)*R_step
+    # azimuthal angle
+    costheta_seed = seed[POS_SEEDS['costheta']]
+    # uniform in [-1,1]  
+    costheta = 2.*costheta_seed - 1.
+    # axial angle
+    phi_seed = seed[POS_SEEDS['phi']]
+    # uniform in [0,2 pi]
+    phi = 2.*np.pi*phi_seed
+    
+    # folding with 3d gaussian
+    gauss_seed_x = seed[POS_SEEDS['gauss_x']]
+    gauss_seed_y = seed[POS_SEEDS['gauss_y']]
+    gauss_seed_z = seed[POS_SEEDS['gauss_z']]
+    # combine three gaussians in quadrature to get '3d' number
+    diffusiveness_seed = np.sqrt(gauss_seed_x*gauss_seed_x  + gauss_seed_y*gauss_seed_y + gauss_seed_z*gauss_seed_z)
+    
+    # diffuse theta function parametrization
+    r = r + diffusiveness_seed*w_gauss;
+    
+    # deformation
+    r, costheta, phi = deform(r,costheta,phi,beta2,beta3)
+    
+    # cartesian coordinates
+    x, y, z = cartesian(r,costheta,phi)
+    
+    return np.array([x,y,z])
+    
+def build_nucleus(seeds_nucleus, n_nucleons, R_step, w_gauss, beta2, beta3):
+
+    nucleus = np.zeros((n_nucleons,3))
+    for n in range(n_nucleons):
+        nucleus[n,:] = place_nucleon(R_step, w_gauss, beta2, beta3, seeds_nucleus[n])
+        
+    return nucleus
+    
+#%%
+def main():
+    conffile = sys.argv[1]
+    with open(conffile, 'r') as stream:
+        confs = yaml.load(stream,Loader=SafeLoader)
+
+    conf_samples = confs['isobar_samples']
+    n_configs = conf_samples['number_configs']['value']
+    n_nucleons = conf_samples['number_nucleons']['value']
+    seeds_file = conf_samples['seeds_file']['filename']
+    out_dir = conf_samples['output_path']['dirname']
+    if(not os.path.isdir(out_dir)):
+        os.mkdir(out_dir)
+    
+    with h5py.File(seeds_file, 'r') as f:
+        seeds = f['isobar_seeds'][:,:,:]
+
+    if seeds.shape[0] < n_configs:
+        n_configs = seeds.shape[0]
+
+    n_isobars = 0
+    isobars = []
+    isobar_names = []
+    # print(confs.keys())
+
+    while ('isobar'+str(n_isobars+1) in confs['isobar_properties'].keys()):
+            isobar_conf = confs['isobar_properties']['isobar'+str(n_isobars+1)]
+            Rstep = isobar_conf['step_radius']['value']
+            diffusiveness = isobar_conf['diffusiveness']['value']
+            beta2 = isobar_conf['beta_2']['value']
+            beta3 = isobar_conf['beta_3']['value']
+            isobars += [ [Rstep,diffusiveness,beta2,beta3] ]
+            isobar_names += [ isobar_conf['isobar_name'] ]
+            n_isobars +=1
+        
+    # print(isobar_names)
+
+    isobars = np.array(isobars)
+    for i in range(n_isobars):
+        data = np.zeros((n_configs,n_nucleons,3),dtype=np.float)
+        for s in range(n_configs):
+            data[s,:,:] = build_nucleus(seeds[s],n_nucleons,*isobars[i])
+    
+        with h5py.File(out_dir+'/'+isobar_names[i]+'.hdf', 'w') as f:
+            data_set = f.create_dataset(isobar_names[i],(n_configs,n_nucleons,3))
+            data_set[:] = data 
+            
+#%%
+if __name__ == "__main__":
+        main()   
---
+++ exec/make_seeds.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 31 16:33:43 2022
+
+@author: mauricio
+
+Usage: ./make_isobar_seeds.py conf.yaml
+
+Creates hdf5 file with shape (number_configs, number_nucleons, 2) 
+with random numbers to generate isobar configurations
+"""
+
+import numpy as np
+import h5py
+import sys
+import yaml
+import numpy.random as random
+from yaml.loader import SafeLoader
+
+N_SEEDS = 3 + 3 # 3 for r, theta, phi + 3 for delta_{x,y,z} Gaussian
+POS_SEEDS = {'radius':0,'costheta':1,'phi':2,'gauss_x':3,'gauss_y':4,'gauss_z':5 }
+GAUSS_SEEDS = ['gauss_x','gauss_y','gauss_z'] # Gaussian seeds
+UNIFORM_SEEDS = ['radius','costheta','phi']
+#%% 
+def seed_nucleon():
+    
+    # radius_seed = random.uniform(low=0.0, high=1.0, size=None)
+    # costheta_seed = random.uniform(low=-1.0, high=1.0, size=None)
+    # phi_seed =  random.uniform(low=0.0, high=2.*np.pi, size=None)
+
+    # diffusiveness_seed_x = random.normal(loc=0.0, scale=1.0, size=None)
+    # diffusiveness_seed_y = random.normal(loc=0.0, scale=1.0, size=None)
+    # diffusiveness_seed_z = random.normal(loc=0.0, scale=1.0, size=None)
+
+    nucleon_seed = np.zeros((N_SEEDS))
+    
+    for seed in UNIFORM_SEEDS:
+        val = random.uniform(low=0.0, high=1.0, size=None)
+        nucleon_seed[POS_SEEDS[seed]] = val
+
+    for seed in GAUSS_SEEDS:
+        val = random.normal(loc=0.0, scale=1.0, size=None)
+        nucleon_seed[POS_SEEDS[seed]] = val        
+
+    return nucleon_seed
+
+def seed_nucleus(n_nucleons):
+
+    nucleus = np.zeros((n_nucleons,N_SEEDS))
+    for n in range(n_nucleons):
+        nucleus[n,:] = seed_nucleon()
+        
+    return nucleus
+    
+#%%
+def main():
+    conffile = sys.argv[1]
+    with open(conffile, 'r') as stream:
+        confs = yaml.load(stream,Loader=SafeLoader)
+
+    conf_seed = confs['isobar_seeds']
+    n_configs = conf_seed['number_configs']['value']
+    n_nucleons = conf_seed['number_nucleons']['value']
+    out_file = conf_seed['output_file']['filename']
+       
+    data = np.zeros((n_configs,n_nucleons,N_SEEDS),dtype=np.float)
+    for i in range(n_configs):
+        data[i,:,:] = seed_nucleus(n_nucleons)
+
+    with h5py.File(out_file, 'w') as f:
+        data_set = f.create_dataset('isobar_seeds',(n_configs,n_nucleons,N_SEEDS))
+        data_set[:] = data 
+#%%
+if __name__ == "__main__":
+        main()   
